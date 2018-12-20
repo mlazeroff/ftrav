4db5a2f1a6e39a212a43ffefa2c800d1215f0253
Matthew Lazeroff <mdlazeroff@gmail.com>
2018-12-19 22:31:55 -0800

    Python Package * Packed Project into Python Package * LICENSE * README * setup.py * ftrav package

---
+++ ftrav/ftrav.py
@@ -0,0 +1,56 @@
+import argparse  # argument parsing
+import os        # path calls
+import ftrav.ftrav_utils as ft
+
+
+def directory_check(path):
+    """
+    Check if passed path is a directory
+    :param path: complete path to the directory
+    :return: path if directory is valid, error if not
+    """
+    if not os.path.isdir(path):
+        raise NotADirectoryError('\"{}\" is not a directory'.format(path))
+
+    return path
+
+
+def file_type_check(file_name):
+    """
+    Check if output file is of XML or JSON type
+    :param file_name: file name
+    :return: xml/json if valid, error if not
+    """
+    if '.' not in file_name:
+        raise TypeError('\"{}\" - file type not given. Must be .xml or .json'.format(file_name))
+
+    # check extension
+    file_type = os.path.split(file_name)[1].split('.')[-1]
+    if file_type != 'xml' and file_type != 'json':
+        raise TypeError('\"{}\" is not of .xml or .json type'.format(file_name))
+
+    return file_type
+
+
+def main():
+    # parse arguments <directory> <hash_type> <report_name>
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory', type=directory_check)
+    parser.add_argument('hash_type', choices=ft.HASH_FXNS)
+    parser.add_argument('report_name', type=str)
+    args = parser.parse_args()
+
+    # traverse directory
+    files = ft.FileParser(args.directory, args.hash_type)
+    files.traverse()
+
+    # output data based on file type
+    file_type = file_type_check(args.report_name)
+    if file_type == 'xml':
+        files.write_xml(args.report_name)
+    elif file_type == 'json':
+        files.write_json(args.report_name)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='ftrav',
+    license='GPL-3',
+    version='0.1dev',
+    author='Matthew Lazeroff',
+    author_email='mdlazeroff@gmail.com',
+    description='Utility for traversing and indexing directory contents',
+    url='https://github.com/mlazeroff/ftrav',
+    packages=['ftrav'],
+    entry_points={'console_scripts': 'ftrav = ftrav.ftrav:main'},
+    classifiers={'Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: GPL-3',
+                 'Operating System :: Linux, OS-X, Windows'}
+)
