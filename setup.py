105ae499203d3d78bc6c7216a6ba4bdae41c3dcc
Matthew Lazeroff <mdlazeroff@gmail.com>
2018-12-19 23:32:36 -0800

    Moved ftrav to scripts/

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description='Utility for traversing and indexing directory contents',
     url='https://github.com/mlazeroff/ftrav',
     packages=['ftrav'],
-    entry_points={'console_scripts': 'ftrav = ftrav.ftrav:main'},
+    entry_points={'console_scripts': 'ftrav = scripts.ftrav:main'},
     classifiers={'Programming Language :: Python :: 3',
                  'License :: OSI Approved :: GPL-3',
                  'Operating System :: Linux, OS-X, Windows'}
